c2187645927f6e11a9c617c7549fc77c18364599
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-16 09:13:02 +0200

    refactor: Dodanie FullName

--- src/RealWorld/Domain/Models/Sensors/Sensor.cs
+++ src/RealWorld/Domain/Models/Sensors/Sensor.cs
@@ -5,8 +5,10 @@ public abstract class Sensor : BaseEntity
     public string Name { get; set; }
     public string IpAddress { get; set; }
 
+    public string FullName => $"{Name} {IpAddress}";
+
     public override string ToString()
     {
-        return $"{Name} {IpAddress}";
+        return FullName;
     }
 }
