27d73700b16d99a1576bce767ab33046673299c9
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-16 08:50:10 +0200

    Dodanie ViewModels

---
+++ src/RealWorld/ViewModels/SensorsViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using Domain.Models.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels;
+
+public abstract class BaseViewModel
+{
+
+}
+
+// Szablon (klasa generyczna)
+public class ItemsViewModel<T> : BaseViewModel
+        where T : Base
+{
+    public List<T> Items { get; set; }
+}
+
+
+
+public class SensorsViewModel : ItemsViewModel<Sensor>
+{
+    public List<Sensor> Sensors => Items;
+}
+
+
+public class CustomersViewModel : ItemsViewModel<Customer>
+{
+}
+
