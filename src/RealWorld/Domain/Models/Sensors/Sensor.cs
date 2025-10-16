d0e931f24374967fbbf87602bdb7748c92f9f451
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-16 09:11:49 +0200

    Wyswietlenie nazwy i adresu ip

--- src/RealWorld/Domain/Models/Sensors/Sensor.cs
+++ src/RealWorld/Domain/Models/Sensors/Sensor.cs
@@ -4,4 +4,9 @@ public abstract class Sensor : BaseEntity
 {
     public string Name { get; set; }
     public string IpAddress { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {IpAddress}";
+    }
 }
