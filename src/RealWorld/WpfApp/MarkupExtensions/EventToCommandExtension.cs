0792c76216fc1a8a98d4d3cda03e6704d823f2c2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-17 11:34:35 +0200

    Dodanie wlasnych markup extensions

---
+++ src/RealWorld/WpfApp/MarkupExtensions/EventToCommandExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace WpfApp.MarkupExtensions;
+
+
+public class HelloExtension : MarkupExtension
+{
+    public string Text { get; set; }
+
+    public HelloExtension()
+    {
+        
+    }
+
+    public HelloExtension(string text)
+    {
+        Text = text;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        if (string.IsNullOrEmpty(Text))
+        {
+            return "Hello custom Markup Extension";
+        }
+        else
+            return $"Hello {Text}";
+    }
+}
+
+public class EventToCommandExtension : MarkupExtension
+{
+    public ICommand Command { get; set; }
+
+    public EventToCommandExtension(ICommand command)
+    {
+        this.Command  = command;
+    }
+
+    // TODO: zaimplementowac
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        throw new NotImplementedException();
+    }
+}
