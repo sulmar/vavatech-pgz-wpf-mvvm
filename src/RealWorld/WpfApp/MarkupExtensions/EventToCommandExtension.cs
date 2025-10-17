e2e22c17657eda504a55f458c897cdffc2d3dcab
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-17 11:35:16 +0200

    Przesuniecie HelloExtension

--- src/RealWorld/WpfApp/MarkupExtensions/EventToCommandExtension.cs
+++ src/RealWorld/WpfApp/MarkupExtensions/EventToCommandExtension.cs
@@ -10,32 +10,6 @@ using System.Windows.Markup;
 
 namespace WpfApp.MarkupExtensions;
 
-
-public class HelloExtension : MarkupExtension
-{
-    public string Text { get; set; }
-
-    public HelloExtension()
-    {
-        
-    }
-
-    public HelloExtension(string text)
-    {
-        Text = text;
-    }
-
-    public override object ProvideValue(IServiceProvider serviceProvider)
-    {
-        if (string.IsNullOrEmpty(Text))
-        {
-            return "Hello custom Markup Extension";
-        }
-        else
-            return $"Hello {Text}";
-    }
-}
-
 public class EventToCommandExtension : MarkupExtension
 {
     public ICommand Command { get; set; }
---
+++ src/RealWorld/WpfApp/MarkupExtensions/HelloExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Markup;
+
+namespace WpfApp.MarkupExtensions;
+
+public class HelloExtension : MarkupExtension
+{
+    public string Text { get; set; }
+
+    public HelloExtension()
+    {
+        
+    }
+
+    public HelloExtension(string text)
+    {
+        Text = text;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        if (string.IsNullOrEmpty(Text))
+        {
+            return "Hello custom Markup Extension";
+        }
+        else
+            return $"Hello {Text}";
+    }
+}
