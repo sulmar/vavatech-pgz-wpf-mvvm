4f52efe9e3ccfa5d2690dc3dcd8eed42d937b5ce
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-21 11:56:07 +0200

    Zastosowanie markup extension do tworzenia convertera

--- src/RealWorld/WpfApp/Converters/CategoryToBrushConverter.cs
+++ src/RealWorld/WpfApp/Converters/CategoryToBrushConverter.cs
@@ -6,21 +6,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Markup;
 using System.Windows.Media;
 
 namespace WpfApp.Converters;
 
-class CategoryToBrushConverter : IValueConverter
+class CategoryToBrushConverter : MarkupExtension, IValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
         if (value is Category category)
         {
             // Match Pattern
-            var color  = category switch
+            var color = category switch
             {
                 Category.P2P => Colors.Violet,
-                Category.Conference => Colors.Blue,   
+                Category.Conference => Colors.Blue,
                 Category.Radionet => Colors.Green,
                 _ => Colors.Red,
             };
@@ -36,4 +37,7 @@ class CategoryToBrushConverter : IValueConverter
     {
         throw new NotImplementedException();
     }
+
+    private static CategoryToBrushConverter _instance;
+    public override object ProvideValue(IServiceProvider serviceProvider) => _instance ??= new CategoryToBrushConverter();
 }
