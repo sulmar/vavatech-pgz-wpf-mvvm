b324729bde9453eab2edf0ac0ff813160b288d20
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-21 11:48:22 +0200

    Dodanie konwertera

---
+++ src/RealWorld/WpfApp/Converters/CategoryToBrushConverter.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp.Converters;
+
+class CategoryToBrushConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is Category category)
+        {
+            // Match Pattern
+            var color  = category switch
+            {
+                Category.P2P => Colors.Violet,
+                Category.Conference => Colors.Blue,   
+                Category.Radionet => Colors.Green,
+                _ => Colors.Red,
+            };
+
+            return new SolidColorBrush(color);
+        }
+
+        return value;
+
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
