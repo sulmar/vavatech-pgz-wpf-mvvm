7dbc0a9dd07ef95701ca00ce7f7ee7f69e1931e0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-10-22 14:46:21 +0200

    3d

--- src/RealWorld/WpfApp/Views/MapSensorsView.xaml.cs
+++ src/RealWorld/WpfApp/Views/MapSensorsView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ViewModels;
@@ -26,6 +27,28 @@ namespace WpfApp.Views
         public MapSensorsView()
         {
             InitializeComponent();
+        }
+
+        private void Viewport3D_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var position = e.GetPosition(viewport);
+            var hitParams = new PointHitTestParameters(position);
+
+            var result =  VisualTreeHelper.HitTest(viewport, position);
+
+            if (result is RayMeshGeometry3DHitTestResult meshHit)
+            {
+                if (meshHit.ModelHit == WallA)
+                {
+
+                }
+                else if (meshHit.ModelHit == WallB)
+                {
+
+                }
+            }
+
+
         }
     }
 }
